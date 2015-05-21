92cb31214363e9f68bdb273346262c19a7316ecc
Dinesh Kumar <dinesh@Dineshs-MacBook-Pro.local>
2015-05-21 11:31:24 +0530

    Initial Commit

---
+++ StackAnswer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackAnswer
+//
+//  Created by Dinesh on 21/05/15.
+//  Copyright (c) 2015 TheBanyanTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
